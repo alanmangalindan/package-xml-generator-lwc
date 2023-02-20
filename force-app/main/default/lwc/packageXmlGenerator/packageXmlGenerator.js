fda875b3dc634945008e17ec40afe11311668dfa
alanm_davanti2 <alan.mangalindan@davanti.co.nz>
2023-02-20 15:17:35 +1300

    updated api version of components

--- force-app/main/default/lwc/packageXmlGenerator/packageXmlGenerator.js
+++ force-app/main/default/lwc/packageXmlGenerator/packageXmlGenerator.js
@@ -8,7 +8,7 @@ export default class PackageXmlGenerator extends LightningElement {
     username = '';
     numberOfDays = '1';
     packageXml = '';
-    apiVersion = '56.0';
+    apiVersion = '57.0';
 
     generatePackage(event) {
         console.log(event.target.label);
